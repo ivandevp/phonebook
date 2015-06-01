6a321dbc3ef749080a64a11ba1b5a3d95ed1949f
Annie Stephannie Azaña Yupanqui <astephannie@gmail.com>
2015-06-01 06:07:07 -0500

    Initial Routes

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,44 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+
+Route::get('createuser', function() 
+{
+	return "Url createuser";
+});
+
+Route::get('updateuser/{user_id}', function($user_id)
+{
+	return "Url updateuser, param: " . $user_id;
+});
+
+Route::get('userdetail/{user_id}', function($user_id)
+{
+	return "Url userdetail, param: " .$user_id;
+});
+
+Route::get('user/{user_id}/createcontact', function($user_id)
+{
+	return "Url user/{user_id}/createcontact, param: " $user_id;
+});
+
+Route::get('user/{user_id}/updatecontact/{contact_id}', function($user_id, $contact_id)
+{
+	return "Url user/{user_id}/updatecontact/{contact_id}, params: " . $user_id . ", " . $contact_id;
+});
+
+Route::get('user/{user_id}/deletecontact/{contact_id}' function($user_id, $contact_id)
+{
+	return "Url user/{user_id}/deletecontact/{contact_id}, params: " . $user_id . ", " . $contact_id;
+});
+
+Route::get('user/{user_id}/contactlist', function($user_id)
+{
+	return "Url user/{user_id}/contactlist, param: " . $user_id;
+});
+
+Route::get('user/{user_id}/contactdetail/{contact_id}', function($contact_id)
+{
+	return "Url user/{user_id}/contactdetail/{contact_id}, param: " . $contact_id;
+});
